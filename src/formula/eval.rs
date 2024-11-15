e09637a8c6b23c2e65fd6b12f9299527056f44ac
Ivan L <tsionyx@gmail.com>
2024-11-15 23:16:37 +0400

    interpretation: tests

--- src/formula/eval.rs
+++ src/formula/eval.rs
@@ -184,3 +184,325 @@ impl<T> Formula<T> {
         }
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use std::sync::atomic::{AtomicU64, Ordering};
+
+    use super::{super::Variable, *};
+
+    static VAR_ID_COUNTER: AtomicU64 = AtomicU64::new(4);
+
+    fn get_var(name: char) -> Variable<char> {
+        let id = match name {
+            'a' => 0,
+            'b' => 1,
+            'c' => 2,
+            'd' => 3,
+            _ => VAR_ID_COUNTER.fetch_add(1, Ordering::Relaxed),
+        };
+
+        Variable::with_data(id, name)
+    }
+
+    fn partial_valuation() -> Valuation<Variable<char>> {
+        let mut val = Valuation::new();
+        val.assign(Arc::new(get_var('a')), true);
+        val.assign(Arc::new(get_var('b')), false);
+        val.assign(Arc::new(get_var('c')), false);
+        val.assign(Arc::new(get_var('d')), true);
+        val
+    }
+
+    #[test]
+    fn tautology() {
+        let f = Formula::tautology();
+        assert_eq!(f.interpret(&partial_valuation()), Formula::TruthValue(true));
+    }
+
+    #[test]
+    fn contradiction() {
+        let f = Formula::contradiction();
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::TruthValue(false)
+        );
+    }
+
+    #[test]
+    fn known_atom() {
+        let f = Formula::atomic(get_var('a'));
+        assert_eq!(f.interpret(&partial_valuation()), Formula::TruthValue(true));
+
+        let f = Formula::atomic(get_var('b'));
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::TruthValue(false)
+        );
+    }
+
+    #[test]
+    fn unknown_atom() {
+        let var = get_var('p');
+        let f = Formula::atomic(var);
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::Atomic(Arc::new(var))
+        );
+    }
+
+    #[test]
+    fn conjunction_known() {
+        let var1 = Arc::new(get_var('a'));
+        let var2 = Arc::new(get_var('b'));
+        let var3 = Arc::new(get_var('c'));
+        let f: Formula<_> = var1.and(var2).and(var3);
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::TruthValue(false)
+        );
+    }
+
+    #[test]
+    fn conjunction_unknown_with_false() {
+        let var1 = Arc::new(get_var('a'));
+        let var2 = Arc::new(get_var('b'));
+        let var3 = Arc::new(get_var('p'));
+        let f: Formula<_> = var1.and(var2).and(var3);
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::TruthValue(false)
+        );
+    }
+
+    #[test]
+    fn conjunction_unknown_with_truths() {
+        let var1 = Arc::new(get_var('a'));
+        let var2 = Arc::new(get_var('d'));
+        let var3 = Arc::new(get_var('p'));
+
+        let f: Formula<_> = var1.clone().and(var2.clone()).and(var3.clone());
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::Atomic(var3.clone())
+        );
+
+        let f: Formula<_> = var2.clone().and(var3.clone()).and(var1.clone());
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::Atomic(var3.clone())
+        );
+
+        let f: Formula<_> = var2.and(var3.clone().and(var1));
+        assert_eq!(f.interpret(&partial_valuation()), Formula::Atomic(var3));
+    }
+
+    #[test]
+    fn conjunction_unknown_with_truths_and_negate() {
+        let var1 = Arc::new(get_var('a'));
+        let var2 = Arc::new(get_var('d'));
+        let var3 = Arc::new(get_var('p'));
+
+        let f: Formula<_> = var1.clone().and(var2.clone()).and(var3.clone().not());
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::Atomic(var3.clone()).not()
+        );
+
+        let f: Formula<_> = var2.clone().and(var3.clone().not()).and(var1.clone());
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::Atomic(var3.clone()).not()
+        );
+
+        let f: Formula<_> = var2.and(var3.clone().not().and(var1));
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::Atomic(var3).not()
+        );
+    }
+
+    #[test]
+    fn disjunction_known() {
+        let var1 = Arc::new(get_var('a'));
+        let var2 = Arc::new(get_var('b'));
+        let var3 = Arc::new(get_var('c'));
+        let f: Formula<_> = var1.or(var2).or(var3);
+        assert_eq!(f.interpret(&partial_valuation()), Formula::TruthValue(true));
+    }
+
+    #[test]
+    fn disjunction_unknown_with_truth() {
+        let var1 = Arc::new(get_var('a'));
+        let var2 = Arc::new(get_var('b'));
+        let var3 = Arc::new(get_var('p'));
+        let f: Formula<_> = var1.or(var2).or(var3);
+        assert_eq!(f.interpret(&partial_valuation()), Formula::TruthValue(true));
+    }
+
+    #[test]
+    fn disjunction_unknown_with_falsitys() {
+        let var1 = Arc::new(get_var('b'));
+        let var2 = Arc::new(get_var('c'));
+        let var3 = Arc::new(get_var('p'));
+
+        let f: Formula<_> = var1.clone().or(var2.clone()).or(var3.clone());
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::Atomic(var3.clone())
+        );
+
+        let f: Formula<_> = var2.clone().or(var3.clone()).or(var1.clone());
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::Atomic(var3.clone())
+        );
+
+        let f: Formula<_> = var2.or(var3.clone().or(var1));
+        assert_eq!(f.interpret(&partial_valuation()), Formula::Atomic(var3));
+    }
+
+    #[test]
+    fn xor_known() {
+        let var1 = Arc::new(get_var('a'));
+        let var2 = Arc::new(get_var('b'));
+        let f: Formula<_> = var1.xor(var2);
+        assert_eq!(f.interpret(&partial_valuation()), Formula::TruthValue(true));
+    }
+
+    #[test]
+    fn xor_unknown_with_different() {
+        let var1 = Arc::new(get_var('a'));
+        let var2 = Arc::new(get_var('b'));
+        let var3 = Arc::new(get_var('p'));
+        let f: Formula<_> = var1.xor(var2).xor(var3.clone());
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::Atomic(var3).not()
+        );
+    }
+
+    #[test]
+    fn xor_unknown_with_same() {
+        let var1 = Arc::new(get_var('a'));
+        let var2 = Arc::new(get_var('d'));
+        let var3 = Arc::new(get_var('p'));
+        let f: Formula<_> = var1.xor(var2).xor(var3.clone());
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::Atomic(var3.clone())
+        );
+
+        let var1 = Arc::new(get_var('b'));
+        let var2 = Arc::new(get_var('c'));
+        let f: Formula<_> = var1.xor(var2).xor(var3.clone());
+        assert_eq!(f.interpret(&partial_valuation()), Formula::Atomic(var3));
+    }
+
+    #[test]
+    fn imply_known() {
+        let var1 = Arc::new(get_var('a'));
+        let var2 = Arc::new(get_var('b'));
+        let f: Formula<_> = var1.clone().implies(var2.clone());
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::TruthValue(false)
+        );
+
+        let f: Formula<_> = var2.implies(var1);
+        assert_eq!(f.interpret(&partial_valuation()), Formula::TruthValue(true));
+    }
+
+    #[test]
+    fn imply_unknown_from_false_vacuous() {
+        let var1 = Arc::new(get_var('b'));
+        let var2 = Arc::new(get_var('p'));
+        let f: Formula<_> = var1.implies(var2);
+        assert_eq!(f.interpret(&partial_valuation()), true.into());
+    }
+
+    #[test]
+    fn imply_unknown_to_false_is_negation() {
+        let var1 = Arc::new(get_var('b'));
+        let var2 = Arc::new(get_var('p'));
+        let f: Formula<_> = var2.clone().implies(var1);
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::Atomic(var2).not()
+        );
+    }
+
+    #[test]
+    fn imply_unknown_from_true_is_identity() {
+        let var1 = Arc::new(get_var('a'));
+        let var2 = Arc::new(get_var('p'));
+        let f: Formula<_> = var1.implies(var2.clone());
+        assert_eq!(f.interpret(&partial_valuation()), Formula::Atomic(var2));
+    }
+
+    #[test]
+    fn imply_unknown_to_true_is_true() {
+        let var1 = Arc::new(get_var('a'));
+        let var2 = Arc::new(get_var('p'));
+        let f: Formula<_> = var2.implies(var1);
+        assert_eq!(f.interpret(&partial_valuation()), true.into());
+    }
+
+    #[test]
+    fn eq_known_same() {
+        let var1 = Arc::new(get_var('c'));
+        let var2 = Arc::new(get_var('b'));
+        let f: Formula<_> = var1.clone().equivalent(var2.clone());
+        assert_eq!(f.interpret(&partial_valuation()), Formula::TruthValue(true));
+
+        let f: Formula<_> = var2.equivalent(var1);
+        assert_eq!(f.interpret(&partial_valuation()), true.into());
+    }
+
+    #[test]
+    fn eq_known_different() {
+        let var1 = Arc::new(get_var('a'));
+        let var2 = Arc::new(get_var('b'));
+        let f: Formula<_> = var1.clone().equivalent(var2.clone());
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::TruthValue(false)
+        );
+
+        let f: Formula<_> = var2.equivalent(var1);
+        assert_eq!(f.interpret(&partial_valuation()), false.into());
+    }
+
+    #[test]
+    fn eq_unknown_with_false_is_negation() {
+        let var1 = Arc::new(get_var('b'));
+        let var2 = Arc::new(get_var('p'));
+
+        let f: Formula<_> = var1.clone().equivalent(var2.clone());
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::Atomic(var2.clone()).not()
+        );
+
+        let f: Formula<_> = var2.clone().equivalent(var1);
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::Atomic(var2).not()
+        );
+    }
+
+    #[test]
+    fn eq_unknown_with_false_is_identity() {
+        let var1 = Arc::new(get_var('a'));
+        let var2 = Arc::new(get_var('p'));
+
+        let f: Formula<_> = var1.clone().equivalent(var2.clone());
+        assert_eq!(
+            f.interpret(&partial_valuation()),
+            Formula::Atomic(var2.clone())
+        );
+
+        let f: Formula<_> = var2.clone().equivalent(var1);
+        assert_eq!(f.interpret(&partial_valuation()), Formula::Atomic(var2));
+    }
+}
