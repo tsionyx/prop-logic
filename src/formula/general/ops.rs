af8b5d140fb32e03cfff66aab2e54d51479c7e19
Ivan L <tsionyx@gmail.com>
2025-06-20 15:58:28 +0500

    Formula: Box ops for ergonomics

--- src/formula/general/ops.rs
+++ src/formula/general/ops.rs
@@ -5,11 +5,30 @@ use super::{
     formula::{Directed, Formula},
 };
 
+impl<T> Not for Box<Formula<T>> {
+    type Output = Formula<T>;
+
+    fn not(self) -> Self::Output {
+        Formula::Not(self)
+    }
+}
+
 impl<T> Not for Formula<T> {
     type Output = Self;
 
     fn not(self) -> Self::Output {
-        Self::Not(Box::new(self))
+        !Box::new(self)
+    }
+}
+
+impl<Rhs, T> BitAnd<Rhs> for Box<Formula<T>>
+where
+    Rhs: Into<Self>,
+{
+    type Output = Formula<T>;
+
+    fn bitand(self, other: Rhs) -> Self::Output {
+        Formula::And(self, other.into())
     }
 }
 
@@ -31,7 +50,26 @@ where
     type Output = Self;
 
     fn bitand(self, other: Rhs) -> Self::Output {
-        Self::And(Box::new(self), Box::new(other.into()))
+        Box::new(self) & other.into()
+    }
+}
+
+impl<T> BitAnd<Box<Self>> for Formula<T> {
+    type Output = Self;
+
+    fn bitand(self, other: Box<Self>) -> Self::Output {
+        Box::new(self) & other
+    }
+}
+
+impl<Rhs, T> BitOr<Rhs> for Box<Formula<T>>
+where
+    Rhs: Into<Self>,
+{
+    type Output = Formula<T>;
+
+    fn bitor(self, other: Rhs) -> Self::Output {
+        Formula::Or(self, other.into())
     }
 }
 
@@ -42,7 +80,26 @@ where
     type Output = Self;
 
     fn bitor(self, other: Rhs) -> Self::Output {
-        Self::Or(Box::new(self), Box::new(other.into()))
+        Box::new(self) | other.into()
+    }
+}
+
+impl<T> BitOr<Box<Self>> for Formula<T> {
+    type Output = Self;
+
+    fn bitor(self, other: Box<Self>) -> Self::Output {
+        Box::new(self) | other
+    }
+}
+
+impl<Rhs, T> BitXor<Rhs> for Box<Formula<T>>
+where
+    Rhs: Into<Self>,
+{
+    type Output = Formula<T>;
+
+    fn bitxor(self, other: Rhs) -> Self::Output {
+        Formula::Xor(self, other.into())
     }
 }
 
@@ -53,7 +110,15 @@ where
     type Output = Self;
 
     fn bitxor(self, other: Rhs) -> Self::Output {
-        Self::Xor(Box::new(self), Box::new(other.into()))
+        Box::new(self) ^ other.into()
+    }
+}
+
+impl<T> BitXor<Box<Self>> for Formula<T> {
+    type Output = Self;
+
+    fn bitxor(self, other: Box<Self>) -> Self::Output {
+        Box::new(self) ^ other
     }
 }
 
@@ -62,7 +127,13 @@ where
     Rhs: Into<Self>,
 {
     fn implies(self, consequent: Rhs) -> Self {
-        Self::Implies(Box::new(self), Box::new(consequent.into()))
+        self.implies(Box::new(consequent.into()))
+    }
+}
+
+impl<T> Implies<Box<Self>> for Formula<T> {
+    fn implies(self, consequent: Box<Self>) -> Self {
+        Self::Implies(Box::new(self), consequent)
     }
 }
 
@@ -71,6 +142,12 @@ where
     Rhs: Into<Self>,
 {
     fn equivalent(self, other: Rhs) -> Self {
-        Self::Equivalent(Box::new(self), Box::new(other.into()))
+        self.equivalent(Box::new(other.into()))
+    }
+}
+
+impl<T> Equivalent<Box<Self>> for Formula<T> {
+    fn equivalent(self, other: Box<Self>) -> Self {
+        Self::Equivalent(Box::new(self), other)
     }
 }
